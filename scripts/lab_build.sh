2b2a2eaddfcf6894aae9862aef2dea3b9921efe4
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-09-20 17:27:33 -0700

    Add a script that builds all lab modules.

---
+++ scripts/lab_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script assumes the kernel has been built within its source tree.
+# See/run kernel_build.sh prior to running this script.
+
+source config.sh
+
+pushd $ELDD_LAB_DIR > /dev/null
+    make KERNEL_DIR=$ELDD_KERNEL_DIR -j$(nproc) all
+popd > /dev/null
