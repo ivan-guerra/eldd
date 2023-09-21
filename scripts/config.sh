88c4bf46cc3070738e02a7c007f13e4e4a275a5b
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-09-20 17:25:51 -0700

    Add script for configuring project paths.

---
+++ scripts/config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+# Root directory.
+ELDD_PROJECT_PATH=$(dirname ${CWD})
+
+# Scripts directory.
+ELDD_SCRIPTS_PATH="${ELDD_PROJECT_PATH}/scripts"
+
+# Kernel source directory.
+ELDD_KERNEL_DIR="${ELDD_PROJECT_PATH}/linux"
+
+# Lab driver directory.
+ELDD_LAB_DIR="${ELDD_PROJECT_PATH}/labs"
